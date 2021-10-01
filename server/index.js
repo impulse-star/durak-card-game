cb39c34e3195dcde74a209089d3184e1b09b74bb
bookshelf <81616418+impulse-star@users.noreply.github.com>
2021-10-01 00:06:49 -0700

    initialized client folder.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const http = require("http");
+const socketIo = require("socket.io");
 const app = express();
-const port = process.env.port || 3001;
+const server = http.createServer(app);
+const io = socketIo(server);
+const port = 3001;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+io.on("connection", (s) => {
+    console.log("Yes i can connect! :)");
+
+    s.on("disconnect", () => {
+        console.log("bye :(");
+    });
 });
 
 app.listen(port, () => {
